6b1fdc17b862e77745877cdce2fedd1f2c80ec54
John Farmer <john.farmer@gmx.net>
2022-02-26 08:28:21 +0100

    Button work with ext. js

---
+++ DeutscheRettungsFlugwacht/main/static/main/main_3.js
@@ -0,0 +1,65 @@
+// timer and date in nav and header/frontpage
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+
+  var date =
+    today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+  document.getElementById("time").innerHTML =
+    date + " | " + h + ":" + m + ":" + s;
+
+  document.getElementById("time_1").innerHTML =
+    date + " | " + h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
+
+// Sonnenaufgang Sonnenuntergang
+// Many thanks and best regards for the sunrise and sunset API.
+// https://sunrise-sunset.org/api
+
+var request = new XMLHttpRequest();
+
+request.open(
+  "GET",
+  "https://api.sunrise-sunset.org/json?lat=54.216737&lng=9.599856&date=today"
+);
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    let sunrise = data["results"]["sunrise"];
+    let sunset = data["results"]["sunset"];
+
+    sunrise = parseFloat(sunrise) + 1 + ":" + sunrise.slice(2, 4);
+    console.log(sunrise);
+    document.getElementById("sunrise").innerHTML = sunrise;
+
+    sunset = parseFloat(sunset) + 13 + ":" + sunset.slice(2, 4);
+    console.log(sunset);
+    document.getElementById("sunset").innerHTML = sunset;
+  } else {
+    console.log("error");
+  }
+};
+
+request.send();
+
+// Notverfahren
+function randomQuestionFlight() {
+  let y = Math.floor(Math.random() * questionArrayFlight.length);
+  let z = y + 1;
+  var x = document.getElementById("flightQues");
+  x.innerHTML = "Frage " + z + ": " + '"' + questionArrayFlight[y] + '"';
+}
